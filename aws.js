db17e3a490a7c93a23b36df6a7ba759148f4ca5b
Dane Petersen <thegreatsunra@gmail.com>
2017-05-30 16:18:35 -0500

    Fix AWS code linting

--- aws.js
+++ aws.js
@@ -13,99 +13,98 @@
  * permissions and limitations under the License.
  */
 
-//node.js deps
+// node.js deps
 
-//npm deps
+// npm deps
 
-//app deps
 const deviceModule = require('..').device;
 const cmdLineProcess = require('./lib/cmdline');
+// app deps
 
-//begin module
+// begin module
 
-function processTest(args) {
-   //
-   // The device module exports an MQTT instance, which will attempt
-   // to connect to the AWS IoT endpoint configured in the arguments.
-   // Once connected, it will emit events which our application can
-   // handle.
-   //
-   const device = deviceModule({
-      keyPath: args.privateKey,
-      certPath: args.clientCert,
-      caPath: args.caCert,
-      clientId: args.clientId,
-      region: args.region,
-      baseReconnectTimeMs: args.baseReconnectTimeMs,
-      keepalive: args.keepAlive,
-      protocol: args.Protocol,
-      port: args.Port,
-      host: args.Host,
-      debug: args.Debug
-   });
+function processTest (args) {
+  //
+  // The device module exports an MQTT instance, which will attempt
+  // to connect to the AWS IoT endpoint configured in the arguments.
+  // Once connected, it will emit events which our application can
+  // handle.
+  //
+  const device = deviceModule({
+    keyPath: args.privateKey,
+    certPath: args.clientCert,
+    caPath: args.caCert,
+    clientId: args.clientId,
+    region: args.region,
+    baseReconnectTimeMs: args.baseReconnectTimeMs,
+    keepalive: args.keepAlive,
+    protocol: args.Protocol,
+    port: args.Port,
+    host: args.Host,
+    debug: args.Debug
+  })
 
-   var timeout;
-   var count = 0;
-   const minimumDelay = 250;
+  let timeout // eslint-disable-line no-unused-vars
+  let count = 0
+  const minimumDelay = 250
 
-   if (args.testMode === 1) {
-      device.subscribe('topic_1');
-   } else {
-      device.subscribe('topic_2');
-   }
-   if ((Math.max(args.delay, minimumDelay)) !== args.delay) {
-      console.log('substituting ' + minimumDelay + 'ms delay for ' + args.delay + 'ms...');
-   }
-   timeout = setInterval(function() {
-      count++;
+  if (args.testMode === 1) {
+    device.subscribe('topic_1')
+  } else {
+    device.subscribe('topic_2')
+  }
+  if ((Math.max(args.delay, minimumDelay)) !== args.delay) {
+    console.log('substituting ' + minimumDelay + 'ms delay for ' + args.delay + 'ms...')
+  }
+  timeout = setInterval(() => {
+    count++
 
-      if (args.testMode === 1) {
-         device.publish('topic_2', JSON.stringify({
-            mode1Process: count
-         }));
-      } else {
-         device.publish('topic_1', JSON.stringify({
-            mode2Process: count
-         }));
-      }
-   }, Math.max(args.delay, minimumDelay)); // clip to minimum
+    if (args.testMode === 1) {
+      device.publish('topic_2', JSON.stringify({
+        mode1Process: count
+      }))
+    } else {
+      device.publish('topic_1', JSON.stringify({
+        mode2Process: count
+      }))
+    }
+  }, Math.max(args.delay, minimumDelay)) // clip to minimum
 
-   //
-   // Do a simple publish/subscribe demo based on the test-mode passed
-   // in the command line arguments.  If test-mode is 1, subscribe to
-   // 'topic_1' and publish to 'topic_2'; otherwise vice versa.  Publish
-   // a message every four seconds.
-   //
-   device
-      .on('connect', function() {
-         console.log('connect');
-      });
-   device
-      .on('close', function() {
-         console.log('close');
-      });
-   device
-      .on('reconnect', function() {
-         console.log('reconnect');
-      });
-   device
-      .on('offline', function() {
-         console.log('offline');
-      });
-   device
-      .on('error', function(error) {
-         console.log('error', error);
-      });
-   device
-      .on('message', function(topic, payload) {
-         console.log('message', topic, payload.toString());
-      });
-
+  //
+  // Do a simple publish/subscribe demo based on the test-mode passed
+  // in the command line arguments.  If test-mode is 1, subscribe to
+  // 'topic_1' and publish to 'topic_2' otherwise vice versa.  Publish
+  // a message every four seconds.
+  //
+  device
+    .on('connect', () => {
+      console.log('connect')
+    })
+  device
+    .on('close', () => {
+      console.log('close')
+    })
+  device
+    .on('reconnect', () => {
+      console.log('reconnect')
+    })
+  device
+    .on('offline', () => {
+      console.log('offline')
+    })
+  device
+    .on('error', (error) => {
+      console.log('error', error)
+    })
+  device
+    .on('message', (topic, payload) => {
+      console.log('message', topic, payload.toString())
+    })
 }
 
-module.exports = cmdLineProcess;
+module.exports = cmdLineProcess
 
 if (require.main === module) {
-   cmdLineProcess('connect to the AWS IoT service and publish/subscribe to topics using MQTT, test modes 1-2',
-      process.argv.slice(2), processTest);
+  cmdLineProcess('connect to the AWS IoT service and publish/subscribe to topics using MQTT, test modes 1-2',
+  process.argv.slice(2), processTest)
 }
